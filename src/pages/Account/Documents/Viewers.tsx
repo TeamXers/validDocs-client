4dbd6eb8938f82093365d7c1e4e01a27bb747538
Eunovo <eunovo9@gmail.com>
2022-04-30 13:05:28 +0100

    WIP: signers and viewers

---
+++ src/pages/Account/Documents/Viewers.tsx
@@ -0,0 +1,9 @@
+import { Paper, Typography } from "@mui/material"
+
+export const Viewers = () => {
+    return <Typography component='div' variant='body2'>
+        <Paper variant='outlined' sx={{ p: 1, mb: 1 }}>AngoJay</Paper>
+        <Paper variant='outlined' sx={{ p: 1, mb: 1 }}>WarriCEO</Paper>
+        <Paper variant='outlined' sx={{ p: 1 }}>Mikkybang</Paper>
+    </Typography>
+}
