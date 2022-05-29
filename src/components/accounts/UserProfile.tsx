9abbe3a777aa8affe46ab1efc731056589085fd6
Eunovo <eunovo9@gmail.com>
2022-05-29 05:14:16 +0100

    Fix blank page before content loads

--- src/components/accounts/UserProfile.tsx
+++ src/components/accounts/UserProfile.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Typography } from "@mui/material"
-import { useState } from "react"
+import { Box, Button, Skeleton, Typography } from "@mui/material"
+import { useMemo, useState } from "react"
 import { useQuery } from "react-query"
 import { GET_DOCUMENTS } from "../../api/validdocs"
 import { Documents } from "../documents/Documents"
@@ -7,32 +7,40 @@ import { SlidingAccountForm } from "./AccountForm"
 import { UserAvatar } from "./UserAvatar"
 
 interface UserProfileProps {
-    account: { username: string, address: string },
+    account?: { username: string, address: string },
     editable?: boolean
 }
 
 export const UserProfile: React.FC<UserProfileProps> = ({ account, editable }) => {
     const [showForm, setShowForm] = useState(false);
+    const query = useMemo(
+        () => ({
+            authorAddress: account?.address,
+            ...(editable ? {} : { isPublic: true })
+        }),
+        [account, editable]
+    );
     const { data: documents, isFetching: documentFetching } = useQuery(
-        ['documents', { authorAddress: account.address, ...(editable ? {} : { isPublic: true }) }],
-        GET_DOCUMENTS, { placeholderData: [] as any });
+        ['documents', query], GET_DOCUMENTS,
+        { enabled: !!account?.address, placeholderData: [] as any });
 
     return <Box>
         <Box display='flex' alignItems='center'>
 
-            <UserAvatar username={account.username} sx={{ fontSize: 36, width: 100, height: 100 }} />
+            {account && <UserAvatar username={account.username} sx={{ fontSize: 36, width: 100, height: 100 }} />}
+            {!account && <Skeleton variant="circular" sx={{ width: 100, height: 100 }} />}
 
             <Box ml={2}>
                 <Box display='flex' alignItems='center'>
                     <Typography variant='h5' sx={{ maxWidth: { xs: 100, sm: 400, md: 600 } }} noWrap>
-                        {account.username}
+                        {account?.username || <Skeleton sx={{ width: 100 }} />}
                     </Typography>
 
                     {editable && <Button onClick={() => setShowForm(true)} sx={{ ml: 4 }}>Change</Button>}
                 </Box>
 
                 <Typography sx={{ maxWidth: { xs: 150, sm: 500, md: 600 } }} noWrap>
-                    {account.address}
+                    {account?.address || <Skeleton />}
                 </Typography>
             </Box>
 
@@ -41,7 +49,7 @@ export const UserProfile: React.FC<UserProfileProps> = ({ account, editable }) =
         <Box mt={6}>
             <Typography variant='h4'>Uploaded Documents</Typography>
 
-            <Documents isLoading={documentFetching} documents={documents} sx={{ mt: 2 }} />
+            <Documents isLoading={documentFetching || !account} documents={documents} sx={{ mt: 2 }} />
         </Box>
 
         <SlidingAccountForm open={showForm} onClose={() => setShowForm(false)} />
